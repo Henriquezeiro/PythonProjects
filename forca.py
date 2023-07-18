8b96e79b53bd890f047797bf8e9d42ff61261ef9
Manoel Oliveira <96673556+Henriquezeiro@users.noreply.github.com>
2023-07-18 17:17:48 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,5 +1,21 @@
 def jogar():
+    global letra
     print("\n************* Jogo da Forca *************\n")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("\nqual a letra: ")
+        index = 1
+        for letra in palavra_secreta:
+            if chute == letra:
+                print("letra {} encontrada na posição {} ".format(letra, index))
+            index += 1
+
+        print("jogando...")
 
 
 if __name__ == "__main__":
