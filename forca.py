2a314a6183518ed5213eda766a7cc259a30166c0
Manoel Oliveira <96673556+Henriquezeiro@users.noreply.github.com>
2023-07-25 15:13:16 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,12 +3,8 @@ import random
 
 def jogar():
     imprimir_mensagem_inicial()
-    carrega_palavra_secreta() # arruma isso depois organizando em funções
-
-    numero_aleatorio = random.randrange(0, len(palavras))  # gera número aleatório para definir como índice da lista
-
-    palavra_secreta = palavras[numero_aleatorio].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    palavra_secreta = carrega_palavra_secreta()  # arruma isso depois organizando em funções
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -18,26 +14,23 @@ def jogar():
     # Enquanto não enforcou e nem acertou o loop continua. É usado not para inverter os valores para TRUE para que o
     # loop possa funcionar
     while not enforcou and not acertou:
-        chute = input("\nqual a letra: ")
-        chute = chute.strip().upper()
+        chute = pede_chute()
 
         if chute in palavra_secreta:  # se o chute estiver em palavra secreta irá ser substitution os underscore
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+            marca_chute_correto(chute, palavra_secreta, letras_acertadas)
+
         else:  # se a letra chutada não estiver na palavra secreta o erro será incrementado em 1
             erros += 1
+            desenha_forca(erros)
 
-        enforcou = erros == 5  # enforcou será True se os erros forem iguais a 5
+        enforcou = erros == 7  # enforcou será True se os erros forem iguais a 5
         acertou = "_" not in letras_acertadas  # acertou será True se os underscores não estiverem em letras_acertadas
         print(letras_acertadas)  # mostra no terminal quais letras estão corretas substituindo os underscores
 
     if acertou:
-        print("Você ganhou!")
+        imprime_mensagem_vencedor()
     else:
-        print("Você perdeu!")
+        imprime_mensagem_perdedor(palavra_secreta)
 
 
 def imprimir_mensagem_inicial():
@@ -55,6 +48,97 @@ def carrega_palavra_secreta():
         palavras.append(linha)
 
     arquivo.close()  # fechar arquivo
+    numero_aleatorio = random.randrange(0, len(palavras))  # gera número aleatório para definir como índice da lista
+    palavra_secreta = palavras[numero_aleatorio].upper()
+
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+
+def pede_chute():
+    chute = input("\nqual a letra: ")
+    chute = chute.strip().upper()
+    return chute
+
+
+def marca_chute_correto(chute, palavra_secreta, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
+    print("   _____ ")
+    print(" ( \\   / )")
+    print("    \\ /")
+    print("    /_\\")
+
+
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Você perdeu!")
+    print("Palavra Secreta:", palavra_secreta)
+    print("    _________")
+    print("  /           \\")
+    print(" |   x     x   |")
+    print("  \\    ^      /")
+    print("     |_|_|_|  ")
+    print("     |-|-|-|")
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+    if erros == 1:
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 2:
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 3:
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 4:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 5:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if erros == 6:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if erros == 7:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if __name__ == "__main__":
